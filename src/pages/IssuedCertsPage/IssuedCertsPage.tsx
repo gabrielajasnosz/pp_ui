946b492c8cf4ae06fa02c37527ebc6fdd25b8e76
gabrielajasnosz <gabriela.jasnosz@gmail.com>
2024-01-06 23:13:30 +0100

    ui improvements

--- src/pages/IssuedCertsPage/IssuedCertsPage.tsx
+++ src/pages/IssuedCertsPage/IssuedCertsPage.tsx
@@ -3,11 +3,16 @@ import { BlockchainService } from '../../ethereum/BlockchainService';
 import { useMetaMask } from '../../hooks/useMetaMask';
 import { CertResponse, Utils } from '../../utils';
 import { Input } from '../../components/Input/Input';
-import { CertItem } from './CertItem/CertItem';
 import {
   CustomSnackbar,
   SnackbarType,
 } from '../../components/CustomSnackbar/CustomSnackbar';
+import "./IssuedCertsPage.scss";
+import Table from '@mui/material/Table';
+import { TableBody, TableCell, TableHead, TableRow } from '@mui/material'
+import IconButton from '@mui/material/IconButton'
+import DeleteIcon from '@mui/icons-material/Delete'
+import moment from 'moment'
 
 export const IssuedCertsPage: React.FC = () => {
   const service = new BlockchainService();
@@ -53,7 +58,7 @@ export const IssuedCertsPage: React.FC = () => {
 
   return (
     <div className={'page-layout'}>
-      <div className={'form-layout'}>
+      <div className={'form-layout certificates'}>
         <h2 className={'form-layout__header'}>List of issued certificates</h2>
         <div className="form-layout__content">
           <Input
@@ -61,14 +66,46 @@ export const IssuedCertsPage: React.FC = () => {
             required={false}
             onChange={(v) => setSearchQuerry(v)}
           />
-          <div>
-            {certificates.filter(includesQuerry(searchQuerry)).map((cert) => (
-              <CertItem
-                key={cert.checksum}
-                cert={cert}
-                onClick={handleDelete}
-              />
-            ))}
+          <div className={'certificates-list'}>
+            <Table sx={{ minWidth: 650, marginTop: '30px' }} size="small" aria-label="a dense table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align='center' sx={{ fontWeight: 'bold'}}>Receiver</TableCell>
+                  <TableCell align='center' sx={{ fontWeight: 'bold'}}>Cert name</TableCell>
+                  <TableCell align='center' sx={{ fontWeight: 'bold'}}>Expiration date</TableCell>
+                  <TableCell align='center' sx={{ fontWeight: 'bold'}}>Checksum</TableCell>
+                  <TableCell align='center' sx={{ fontWeight: 'bold'}}>Delete</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {certificates.filter(includesQuerry(searchQuerry)).map((cert) => (
+                  <TableRow
+                    key={cert.checksum}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 }, height: '50px !important' }}
+                  >
+                    <TableCell align='center'>
+                      {cert.firstName} {cert.secondName}
+                    </TableCell>
+                    <TableCell align='center'>{cert.certName}</TableCell>
+                    <TableCell align='center'>{moment(cert.expireDate).format('DD-MM-YYYY')}</TableCell>
+                    <TableCell align='center' sx={{ display: 'flex', justifyContent: 'center' }}>
+                      <div className={'checksum-cell'}>
+                        {cert.checksum}
+                      </div>
+                      </TableCell>
+                    <TableCell align='center'>
+                      <IconButton
+                      aria-label="delete"
+                      sx={{ padding: '0px !important'}}
+                      onClick={() => handleDelete(cert.checksum)}
+                    >
+                      <DeleteIcon sx={{ fontSize: '18px', padding: '0px !important'}} />
+                    </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
           </div>
         </div>
       </div>
